9fcea2114e8b81254f6a42c38b8a328cece6fcac
Sean Breckenridge <seanbrecke@gmail.com>
2023-03-06 02:18:58 -0800

    init

---
+++ README.md
@@ -0,0 +1,45 @@
+# m3u-shuf
+
+This supports a subset of the extended m3u format - requires the `#EXTM3U` header and keeps `#EXTINF` attached to each entry while shuffing.
+
+```
+CLI tool to shuffle a m3u file. If no file given, reads from STDIN
+
+Usage: m3u-shuf [OPTIONS] [FILE]
+
+Arguments:
+  [FILE]
+          m3u file to shuffle
+
+Options:
+  -o, --output <OUTPUT>
+
+
+  -h, --help
+          Print help (see a summary with '-h')
+
+  -V, --version
+          Print version
+```
+
+### Install
+
+Install [cargo](https://doc.rust-lang.org/cargo/getting-started/installation.html), then:
+
+```
+cargo install --git https://github.com/seanbreckenridge/m3u-shuf
+```
+
+#### Example Usage
+
+`m3u-shuf playlist.m3u > shuffled.m3u`
+
+`m3u-shuf playlist.m3u -o shuffled.m3u`
+
+This supports shuffling in place, e.g.:
+
+`m3u-shuf playlist.m3u -o playlist.m3u`
+
+... and reading from STDIN:
+
+`cat playlist.m3u | m3u-shuf | tee shuffled.m3u`
---
+++ src/main.rs
@@ -0,0 +1,110 @@
+use std::fmt;
+use std::fs;
+use std::io;
+use std::str::FromStr;
+
+use rand::seq::SliceRandom;
+use rand::thread_rng;
+
+use anyhow::{bail, Result};
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(
+    author,
+    version,
+    about,
+    long_about = "CLI tool to shuffle a m3u file. If no file given, reads from STDIN"
+)]
+struct Cli {
+    /// m3u file to shuffle
+    file: Option<String>,
+
+    // output file to write to. If not given, writes to STDOUT
+    #[clap(short, long)]
+    output: Option<String>,
+}
+
+// note: these contain newlines
+struct Track {
+    extinf: Option<String>,
+    path: String,
+}
+
+impl fmt::Display for Track {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        if let Some(extinf) = &self.extinf {
+            writeln!(f, "{}", extinf)?;
+        }
+        write!(f, "{}", self.path)
+    }
+}
+
+struct M3U {
+    pub tracks: Vec<Track>,
+}
+
+const EXTM3U: &str = "#EXTM3U";
+const EXTINF: &str = "#EXTINF";
+
+impl FromStr for M3U {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut lines = s.lines();
+        // make sure the first line is the header
+        if !lines.next().unwrap_or_default().starts_with(EXTM3U) {
+            bail!("Invalid header")
+        }
+        let mut tracks = Vec::new();
+        let mut extinf = None;
+        for line in lines {
+            if line.trim().is_empty() {
+                continue;
+            } else if line.starts_with(EXTINF) {
+                extinf = Some(line.trim_end().to_string());
+            } else {
+                tracks.push(Track {
+                    extinf,
+                    path: line.trim_end().to_string(),
+                });
+                extinf = None;
+            }
+        }
+        Ok(M3U { tracks })
+    }
+}
+
+impl fmt::Display for M3U {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(f, "{}", EXTM3U)?;
+        for track in &self.tracks {
+            writeln!(f, "{}", track)?;
+        }
+        Ok(())
+    }
+}
+
+fn main() -> Result<()> {
+    let args = Cli::parse();
+
+    // if args.file is None, read from STDIN
+    let buffer = match args.file {
+        Some(ref file) => fs::read_to_string(file)?,
+        None => io::read_to_string(io::stdin())?,
+    };
+
+    // parse
+    let mut tracks = buffer.parse::<M3U>()?.tracks;
+    tracks.shuffle(&mut thread_rng());
+
+    let out = M3U { tracks }.to_string();
+
+    // write to STDOUT
+    match args.output {
+        Some(ref file) => fs::write(file, out)?,
+        None => print!("{}", out),
+    }
+
+    Ok(())
+}
